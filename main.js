e2e04149bc8e3cb2f579c87bec3fe6a51a8e1351
Anh-Thu Phan <aphan@ufl.edu>
2021-02-12 23:08:57 -0700

    Create pizza object with 3 properties and 1 method

---
+++ main.js
@@ -0,0 +1,9 @@
+var pizza = {
+    crust: thin,
+    cheese: mozzarella,
+    sauce: marinara,
+    addToppings:
+        function () {
+            console.log("Please add pepperoni and sausage");
+        }
+};
